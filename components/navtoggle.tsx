e806b959f6e947374e1b292230f17bda9d2aedde
unknown <yanceevillanueva16@gmail.com>
2023-04-11 14:53:20 +0800

    fix

--- components/navtoggle.tsx
+++ components/navtoggle.tsx
@@ -1,6 +1,12 @@
 import { motion } from "framer-motion";
 
-const Path = ({ openPath, closedPath, ...rest }) => {
+interface PathProps {
+  openPath: string;
+  closedPath: string;
+  [key: string]: any; // Additional props passed to motion.path
+}
+
+const Path: React.FC<PathProps> = ({ openPath, closedPath, ...rest }) => {
   return (
     <motion.path
       fill="transparent"
@@ -16,9 +22,16 @@ const Path = ({ openPath, closedPath, ...rest }) => {
   );
 };
 
-const MenuToggler = ({ toggle }) => {
+interface MenuTogglerProps {
+  toggle: () => void;
+}
+
+const MenuToggler: React.FC<MenuTogglerProps> = ({ toggle }) => {
   return (
-    <button className={"border-0 outline-none focus:outline-none absolute top-5 right-5 w-50 h-50 rounded-full;"}onClick={toggle}>
+    <button
+      className={"border-0 outline-none focus:outline-none absolute top-5 right-5 w-50 h-50 rounded-full;"}
+      onClick={toggle}
+    >
       <svg width="23" height="23" viewBox="0 0 23 23">
         <Path openPath="M 3 16.5 L 17 2.5" closedPath="M 2 2.5 L 20 2.5" />
         <Path closedPath="M 2 9.423 L 20 9.423" openPath="" />
@@ -29,3 +42,36 @@ const MenuToggler = ({ toggle }) => {
 };
 
 export default MenuToggler;
+
+
+// import { motion } from "framer-motion";
+
+// const Path = ({ openPath, closedPath, ...rest }) => {
+//   return (
+//     <motion.path
+//       fill="transparent"
+//       strokeWidth="3"
+//       stroke="hsl(0, 0%, 100%)"
+//       strokeLinecap="round"
+//       variants={{
+//         open: { d: openPath, transition: { duration: 0.3 } },
+//         closed: { d: closedPath, transition: { duration: 0.3 } },
+//       }}
+//       {...rest}
+//     ></motion.path>
+//   );
+// };
+
+// const MenuToggler = ({ toggle }) => {
+//   return (
+//     <button className={"border-0 outline-none focus:outline-none absolute top-5 right-5 w-50 h-50 rounded-full;"}onClick={toggle}>
+//       <svg width="23" height="23" viewBox="0 0 23 23">
+//         <Path openPath="M 3 16.5 L 17 2.5" closedPath="M 2 2.5 L 20 2.5" />
+//         <Path closedPath="M 2 9.423 L 20 9.423" openPath="" />
+//         <Path closedPath="M 2 16.346 L 20 16.346" openPath="M 3 2.5 L 17 16.346" />
+//       </svg>
+//     </button>
+//   );
+// };
+
+// export default MenuToggler;
